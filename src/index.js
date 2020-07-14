28c9e9aa4e16fa5cddfe7b85699d496b82b810f9
Alin Chindea <alin.chindea@gmail.com>
2020-07-14 21:07:39 +0100

    Move app to standalone repo

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Router from './components/Router';
+import './css/style.css';
+
+require('dotenv').config();
+
+render(<Router />, document.querySelector('#main'));
